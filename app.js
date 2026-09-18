c88614ac5c53d4854c6c6bfcda1f849e0e849172
Hussain <hussainshahhashmi5@gmail.com>
2026-09-18 03:49:00 -0700

    fixed delete button

--- app.js
+++ app.js
@@ -579,7 +579,10 @@ function createTileElement(canvasX, canvasY, tileType = 'foreground') {
   // Delete Button
   const deleteBtn = document.createElement('div');
   deleteBtn.className = 'tile-delete';
-  deleteBtn.innerHTML = '×';
+  // Replace innerHTML = "×" or innerText = "×" with this:
+deleteBtn.innerHTML = `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M18 6L6 18M6 6l12 12"/>
+</svg>`;
   deleteBtn.addEventListener('click', (e) => {
     e.stopPropagation();
     tile.remove();
